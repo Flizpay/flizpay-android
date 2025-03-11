c16f8773e4cfc961ee2dddbb766fe4062ce9ec8e
Thrashattack <henrique2092@gmail.com>
2025-03-11 18:07:38 +0100

    fix: unify all logic of webview

--- Android/FlizpaySDK/flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/lib/WebViewService.kt
+++ Android/FlizpaySDK/flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/lib/WebViewService.kt
@@ -1,52 +1,43 @@
 package flizpay2.flizpaysdk.lib
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.webkit.WebChromeClient
-import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class WebViewService : AppCompatActivity() {
-    private var webView: WebView? = null
-    private var redirectUrl: Uri? = null
     private lateinit var keychainService: KeychainService
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        webView = WebView(this)
-        setContentView(webView)
 
-        webView?.settings?.javaScriptEnabled = true
-        webView?.settings?.domStorageEnabled = true
-        webView?.webChromeClient = WebChromeClient()
-        webView?.webViewClient = WebViewClient()
-    }
+        // Get Intent Data
+        val redirectUrl = intent.getStringExtra("redirectUrl") ?: return
+        val token = intent.getStringExtra("token") ?: return
+        val email = intent.getStringExtra("email") ?: return
+        val keychainAccessKey = intent.getStringExtra("keychainAccessKey") ?: "flizpay_keychain_access_key"
 
-    /**
-     * Presents the Flizpay web view modally using a redirect URL.
-     * @param redirectUrl The URL to which the web view should navigate.
-     * @param token JWT token for authentication.
-     * @param email User's email for the transaction.
-     */
-    fun present(
-        redirectUrl: String,
-        token: String,
-        email: String,
-        keychainAccessKey: String
-    ) {
-        val redirectUrlWithJwtToken = "$redirectUrl&jwttoken=$token&email=$email"
-        println("URL is $redirectUrlWithJwtToken")
-        
-        this.redirectUrl = Uri.parse(redirectUrlWithJwtToken)
+        // Instantiate Keychain Service
         keychainService = KeychainService(this, keychainAccessKey)
-        val webViewBridge = WebViewBridge(keychainService, webView as WebView)
 
-        webView?.addJavascriptInterface(webViewBridge, "KeychainBridge")
-        webView?.loadUrl(redirectUrlWithJwtToken)
+        // Instantiate WebView
+        val webView = WebView(this).apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            webChromeClient = WebChromeClient()
+            webViewClient = WebViewClient()
+        }
+        // Set content
+        setContentView(webView)
+        // Add keychain bridge
+        webView.addJavascriptInterface(WebViewBridge(keychainService, webView), "KeychainBridge")
+
+        // Load redirect url
+        val redirectUrlWithJwtToken = "$redirectUrl&jwt=$token&email=$email"
+        println("URL is $redirectUrlWithJwtToken")
+        webView.loadUrl(redirectUrlWithJwtToken)
     }
 }
