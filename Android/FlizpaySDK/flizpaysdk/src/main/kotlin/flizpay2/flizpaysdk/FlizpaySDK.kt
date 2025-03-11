665e5f1a3e8f4f186a39ff81707d1edff1c043a9
Thrashattack <henrique2092@gmail.com>
2025-03-11 18:07:23 +0100

    fix: call intent to render webview

--- Android/FlizpaySDK/flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/FlizpaySDK.kt
+++ Android/FlizpaySDK/flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/FlizpaySDK.kt
@@ -1,6 +1,8 @@
 package flizpay2.flizpaysdk
 
+
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import flizpay2.flizpaysdk.lib.TransactionService
 import flizpay2.flizpaysdk.lib.WebViewService
@@ -31,18 +33,26 @@ object FlizpaySDK {
             if(result.isSuccess) {
                 val redirectUrl = result.getOrNull()?.redirectUrl ?: Constants.BASE_URL
 
-                if (context is AppCompatActivity) {
-                    WebViewService()
-                        .present(
-                                keychainAccessKey = keychainAccessKey ?: "flizpay_keychain_access_key",
-                                redirectUrl = redirectUrl,
-                                token = token,
-                                email = email
-                        )
-                }
-            } else
-                onFailure?.invoke(result.exceptionOrNull() ?: Throwable("Unknown Result"))
+                println("Trying to start webview")
 
+                val intent = Intent(context, WebViewService::class.java).apply {
+                    putExtra("redirectUrl", redirectUrl)
+                    putExtra("token", token)
+                    putExtra("email", email)
+                    putExtra("keychainAccessKey", keychainAccessKey)
+                }
+
+                // Ensure it's an activity context
+                if (context !is AppCompatActivity) {
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)  // Needed for non-Activity context
+                }
+
+                context.startActivity(intent)
+            } else {
+                val error = result.exceptionOrNull()?.message ?: "Unknown error"
+                println("TransactionService failed: $error")
+                onFailure?.invoke(result.exceptionOrNull() ?: Throwable("Unknown Result"))
+            }
         }
     }
 }
