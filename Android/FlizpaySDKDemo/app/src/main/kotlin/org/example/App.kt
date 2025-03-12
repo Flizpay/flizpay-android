2e577cee85e6c22984269674824776d4da860bd2
Thrashattack <henrique2092@gmail.com>
2025-03-12 16:32:46 +0100

    fix: remove email from the call to sdk and adjust button

--- Android/FlizpaySDKDemo/app/src/main/kotlin/org/example/App.kt
+++ Android/FlizpaySDKDemo/app/src/main/kotlin/org/example/App.kt
@@ -3,7 +3,6 @@ package org.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
