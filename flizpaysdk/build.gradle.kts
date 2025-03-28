b7ba2cbf290c3739605cb0ea9d28288e765a0d03
Thrashattack <henrique2092@gmail.com>
2025-03-28 17:30:01 +0100

    fix: pass correct paths to coveralls step

--- flizpaysdk/build.gradle.kts
+++ flizpaysdk/build.gradle.kts
@@ -129,11 +129,9 @@ tasks.register<JacocoReport>("jacocoTestReport") {
     reports {
         xml.apply {
             isEnabled = true
-            outputLocation.set(file("build/reports/jacoco/jacoco.xml"))
         }
         html.apply {
             isEnabled = true
-            outputLocation.set(file("build/reports/jacoco/html"))
         }
     }
 
