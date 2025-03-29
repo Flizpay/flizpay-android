3ed40ed3b2930f3d6a76af6ef4a48b8026856232
Michel Mendes <107858123+michelmnds@users.noreply.github.com>
2025-03-29 16:37:29 +0000

    fix: open external apps (#18)
    * fix: open external apps
    
    * feat: implement new manifest
    
    * fix: revert android plufin version
    
    * fix: revert constants
    
    * feat: add urlScheme to contants + remove unecessary code
    
    * fix: move banks to constants and use var in constants
    
    ---------
    
    Co-authored-by: Thrashattack <henrique2092@gmail.com>

--- flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/Constants.kt
+++ flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/Constants.kt
@@ -3,4 +3,14 @@ package flizpay2.flizpaysdk
 object Constants {
     var API_URL: String = "https://api.flizpay.de"
     var BASE_URL: String = "https://secure.flizpay.de"
+    var URL_SCHEME: String = "flizpaywebview://"
+    var NO_CREDS_BANKS = listOf(
+        "ing-diba.de",    // ING-DiBa
+        "revolut.com",    // Revolut
+        "consorsbank.de", // Consorsbank
+        "n26.com",        // N26
+        "tomorrow.one",   // Tomorrow
+        "kontist.com",    // Kontist
+        "finom.com"       // Finom
+    )
 }
--- flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/lib/WebViewService.kt
+++ flizpaysdk/src/main/kotlin/flizpay2/flizpaysdk/lib/WebViewService.kt
@@ -1,20 +1,24 @@
 package flizpay2.flizpaysdk.lib
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import flizpay2.flizpaysdk.Constants
+
 
 class WebViewService : AppCompatActivity() {
-
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Get Intent Data
         val redirectUrl = intent.getStringExtra("redirectUrl") ?: return
+        val urlScheme = Constants.URL_SCHEME
         val token = intent.getStringExtra("token") ?: return
 
         // Instantiate WebView
@@ -25,18 +29,44 @@ class WebViewService : AppCompatActivity() {
         // Register WebViewBridge to intercept window.close()
         val webViewBridge = WebViewBridge(webView, this)
 
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val url = request?.url ?: return false
+                val host = url.host ?: return false
+
+                // If the URL is HTTPS and host is in the noCredentialsBankHosts list,
+                // open it externally instead of loading in WebView
+                if (url.scheme.equals("https", ignoreCase = true) &&
+                    Constants.NO_CREDS_BANKS.any { host.contains(it) }
+                ) {
+                    val intent = Intent(Intent.ACTION_VIEW, url)
+                    if (intent.resolveActivity(packageManager) != null) {
+                        startActivity(intent)
+                    }
+                    return true // Cancel loading inside the WebView
+                }
+                return false // Allow the WebView to load the URL
+            }
+        }
+
+
         runOnUiThread {
             webView.webChromeClient = WebChromeClient()
-            webView.webViewClient = WebViewClient()
-
+       
             // Set content
             setContentView(webView)
 
             // Load redirect URL with token
-            val redirectUrlWithJwtToken = "$redirectUrl&jwt=$token"
+             val redirectUrlWithJwtToken = "$redirectUrl&jwt=$token&redirect-url=$urlScheme"
+
+            // Load the URL
             webView.loadUrl(redirectUrlWithJwtToken)
 
-            // Override window.close behavior
+            // Override window.close behavior if needed
             webViewBridge.overrideWindowClose()
         }
     }
